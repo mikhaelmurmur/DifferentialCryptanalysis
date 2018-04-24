ef221acdf68ce986eb0d131796a61972c0392d8e
Mykhailo Stolovych <mstolovych@outlook.com>
2018-04-24 14:52:15 +0300

    added google tests

---
+++ CypherTests/HeysTests.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+
+TEST(HeysTests, EmptyFile)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(HeysTests, OnlyOne)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(HeysTests, LessThanNSquareSize)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(HeysTests, RandomInput)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
